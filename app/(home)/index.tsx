7374cd91b812fd893816c335483bf99ca087d42c
looju <omofade2020@gmail.com>
2024-12-10 19:32:44 +0100

    assets for coins, call endpoint

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -5,13 +5,29 @@ import {
   View,
   useColorScheme,
 } from "react-native";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import CustomHeader from "@/components/CustomHeader";
 import { ThemedView } from "@/components/ThemedView";
 import Colors from "@/constants/Colors";
+import axios from "axios";
+import { Base_url } from "@/constants/BaseUrl";
 
 const MainScreen = () => {
   const colors = useColorScheme();
+  const [coinData, setCoinData] = useState();
+  const [coinName, setCoinName] = useState("btc-bitcoin");
+
+  useEffect(() => {
+    const config = {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+    axios.get(`${Base_url}/tickers/${coinName}`, config).then((response) => {
+      console.log(response.data);
+      setCoinData(response.data);
+    });
+  });
   return (
     <SafeAreaView
       style={{
---
+++ constants/Paits.ts
@@ -0,0 +1,27 @@
+const pairs = [
+  {
+    label: "BTC/USD",
+    value: "bitcoin",
+    image: require("@/assets/images/pairs/png"),
+  },
+  {
+    label: "ETH/USD",
+    value: "ethereum",
+    image: require("@/assets/images/eth.png"),
+  },
+  {
+    label: "TETHER/USD",
+    value: "tether",
+    image: require("@/assets/images/tether.jpg"),
+  },
+  {
+    label: "RIPPLE/USD",
+    value: "ripple",
+    image: require("@/assets/images/xrp.jpeg"),
+  },
+  {
+    label: "CARDANO/USD",
+    value: "cardano",
+    image: require("@/assets/images/cardano.jpeg"),
+  },
+];
